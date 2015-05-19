51e5cecfd64e0b98009d9579e702df230d203f2a
Amanda McCumber <Serenity333@users.noreply.github.com>
2015-05-19 07:17:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,5 @@ $(document).ready(function() {
     $('#ball').click(function() {
         $('#ball').slideUp("slow");
     });
-    $('#ballb').click(function() {
-        $('#ballb').bounce({time:5}, 1000);
-    });
-    $('#ballc').click(function() {
-        $('#ballc').bounce({time:3}, 500);
-    });
-    $('#balld').click(function() {
-        $('#balld').slideDown("fast");
-    });
+    
    });
