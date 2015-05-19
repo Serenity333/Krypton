1f312ee923726759a92208cbefbf0df4642f8e18
Amanda McCumber <Serenity333@users.noreply.github.com>
2015-05-18 22:37:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $('#ball').click(function() {
         $('#ball').effect('slide');
     });
-    $('#ball2').click(function() {
-        $('#ball2').effect('bounce', {time:5}, 1000);
+    $('#ballb').click(function() {
+        $('#ballb').effect('bounce', {time:5}, 1000);
     });
-    $('#ball3').click(function() {
-        $('#ball3').effect('bounce', {time:3}, 500);
+    $('#ballc').click(function() {
+        $('#ballc').effect('bounce', {time:3}, 500);
     });
-    $('#ball4').click(function() {
-        $('#ball4').effect('slide');
+    $('#balld').click(function() {
+        $('#balld').effect('slide');
     });
    });
